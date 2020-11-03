0a761bd47d8318036e5bd151b5136723578f7c4c
code7cs <code7cs@gmail.com>
2020-11-03 10:17:52 -0500

    1

---
+++ NodeJS/Mock/showImage/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+let path = require('path');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hello World');
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000, function() {
+	console.log('Example app listening on port 3000');
+});
