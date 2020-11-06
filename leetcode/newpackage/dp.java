c5fb4e3553fa10591dc832934ac673b267fe3979
code7cs <code7cs@gmail.com>
2020-11-05 23:20:44 -0500

    1

---
+++ leetcode/newpackage/dp.java
@@ -0,0 +1,38 @@
+package newpackage;
+
+import java.util.Arrays;
+
+class Solution {
+    public static int[] solution(int[] A) {
+        // write your code in Java SE 8
+        int[][] D = new int[A.length * 2 + 1][2];
+
+        for (int i = 0; i < A.length; i++) {
+            D[A[i]][0]++;
+            D[A[i]][1] = -1;
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            if (D[A[i]][1] == -1) {
+                D[A[i]][1] = 0;
+                for (int j = 1; j <= Math.sqrt(A[i]); j++) {
+                    if (A[i] % j == 0 && A[i] / j != j) {
+                        D[A[i]][1] += D[j][0];
+                        D[A[i]][1] += D[A[i] / j][0];
+                    } else if (A[i] % j == 0 && A[i] / j == j) {
+                        D[A[i]][1] += D[j][0];
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A.length - D[A[i]][1];
+        }
+        return A;
+    }
+
+    public static void main(String[] args) {
+        int[] A = { 3, 1, 2, 3, 6 };
+        System.out.println(Arrays.toString(solution(A)));
+    }
+}
