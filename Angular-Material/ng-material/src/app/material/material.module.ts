4c88943b7e1eaa8fcf6af8f7a4e1a60f8b3df003
code7cs <code7cs@gmail.com>
2020-11-07 11:50:28 -0500

    sidenav

--- Angular-Material/ng-material/src/app/material/material.module.ts
+++ Angular-Material/ng-material/src/app/material/material.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 const material = [
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatSidenavModule
 ]
 
 @NgModule({
