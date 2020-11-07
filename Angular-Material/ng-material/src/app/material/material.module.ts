35c7a9c6a9b8ff00031b22eed960c07ab1acfa00
code7cs <code7cs@gmail.com>
2020-11-07 11:23:30 -0500

    1

---
+++ Angular-Material/ng-material/src/app/material/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+const material = [
+  MatButtonModule,
+  MatToolbarModule
+]
+
+@NgModule({
+  // declarations: [],
+  imports: [
+    // CommonModule
+    material
+  ],
+  exports: [
+    material
+  ]
+})
+export class MaterialModule { }
+
