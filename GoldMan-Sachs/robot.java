e21499daa3f3c9e460f76841d818d70498b66f0f
code7cs <code7cs@gmail.com>
2020-11-18 15:18:14 -0500

    robot

---
+++ GoldMan-Sachs/robot.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class robot {
+    static int robotSim(int[] commands, int[][] obstacles) {
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        // Set of obstacles indexes in the format of : obstacle[0] + " " + obstacle[1]
+        Set<String> obstaclesSet = new HashSet<>();
+        for (int[] obstacle : obstacles) {
+            obstaclesSet.add(obstacle[0] + " " + obstacle[1]);
+        }
+
+        int x = 0, y = 0, direction = 0, maxDistSquare = 0;
+        for (int i = 0; i < commands.length; i++) {
+            if (commands[i] == -2) { // Turns left
+                direction = (direction + 3) % 4;
+            } else if (commands[i] == -1) { // Turns right
+                direction = (direction + 1) % 4;
+            } else { // Moves forward commands[i] steps
+                int step = 0;
+                while (step < commands[i]
+                        && (!obstaclesSet.contains(
+                        (x + directions[direction][0]) + " " + (y + directions[direction][1]))
+                )
+                ) {
+                    x += directions[direction][0];
+                    y += directions[direction][1];
+                    step++;
+                }
+            }
+            maxDistSquare = Math.max(maxDistSquare, x * x + y * y);
+        }
+
+        return maxDistSquare;
+    }
+
+    public static void main(String[] args) {
+        int res = robotSim(new int[]{4, -1, 3}, new int[][]{});
+        System.out.println(res);
+    }
+}
--- NodeJS/Mock/showImage/app.js
+++ NodeJS/Mock/showImage/app.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res) {
 	res.send('Hello World');
 });
 
-app.use('/image',express.static(path.join(__dirname, 'public')));
+app.use('/image', express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000');
