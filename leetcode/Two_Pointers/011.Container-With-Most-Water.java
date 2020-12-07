b65e16b8456ddbc1360b2f40e85a204a3371d2a0
code7cs <code7cs@gmail.com>
2020-12-07 15:44:34 -0500

    11

---
+++ leetcode/Two_Pointers/011.Container-With-Most-Water.java
@@ -0,0 +1,21 @@
+package Two_Pointers;
+
+class Solutoin {
+    public int maxArea(int[] height) {
+        
+        int left = 0, right = height.length - 1;
+        int max = 0;
+        
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                max = Math.max(max, (right - left) * height[left]);
+                left++;
+            } else {
+                max = Math.max(max, (right - left) * height[right]);
+                right--;
+            }
+        }
+        
+        return max;
+    }
+}
