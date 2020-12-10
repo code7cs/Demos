1fcd8e94855e78c104fa397d61d93eb790e7e5bd
code7cs <code7cs@gmail.com>
2020-12-09 23:30:54 -0500

    dfs

--- leetcode/DFS/695.Max-Area-of-Island(M).java
+++ leetcode/DFS/695.Max-Area-of-Island(M).java
@@ -5,16 +5,6 @@ class Solution {
     private int[][] g;
     private boolean[][] visited;
 
-    private int dfs(int i, int j) {
-        if (i < 0 || i >= I || j < 0 || j >= J || visited[i][j] || g[i][j] == 0)
-            return 0;
-
-        visited[i][j] = true;
-
-        return dfs(i - 1, j) + dfs(i + 1, j) + 
-                   dfs(i, j - 1) + dfs(i, j + 1) + 1;
-    }
-
     public int maxAreaOfIsland(int[][] grid) {
         g = grid;
         I = g.length;
@@ -26,5 +16,14 @@ class Solution {
                 if (!visited[i][j] && g[i][j] == 1)
                     maxArea = Math.max(maxArea, dfs(i, j));
         return maxArea;
+    }
+
+    private int dfs(int i, int j) {
+        if (i < 0 || i >= I || j < 0 || j >= J || visited[i][j] || g[i][j] == 0)
+            return 0;
+
+        visited[i][j] = true;
+
+        return dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i, j + 1) + 1;
     }
 }
