5f95ae9df52e1b7d8dc8bbb730af2157d24bb15a
code7cs <code7cs@gmail.com>
2020-12-11 00:51:34 -0500

    DFS

---
+++ leetcode/DFS/130. Surrounded Regions.java
@@ -0,0 +1,45 @@
+package DFS;
+
+class Solution3 {
+    private int m, n;
+    private int[][] direction = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+    public void solve(char[][] board) {
+
+        if (board == null || board.length == 0)
+            return;
+        m = board.length;
+        n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O')
+                dfs(board, i, 0);
+            if (board[i][n - 1] == 'O')
+                dfs(board, i, n - 1);
+        }
+        for (int j = 1; j < n - 1; j++) {
+            if (board[0][j] == 'O')
+                dfs(board, 0, j);
+            if (board[m - 1][j] == 'O')
+                dfs(board, m - 1, j);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+                if (board[i][j] == '*')
+                    board[i][j] = 'O';
+            }
+        }
+        return;
+
+    }
+
+    public void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O')
+            return;
+        board[i][j] = '*';
+        for (int[] d : direction) {
+            dfs(board, i + d[0], j + d[1]);
+        }
+    }
+}
