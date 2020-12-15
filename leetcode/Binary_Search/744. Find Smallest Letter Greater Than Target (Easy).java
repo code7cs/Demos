51886ed608d143d27f47a7825598e2b86fd3d295
code7cs <code7cs@gmail.com>
2020-12-14 23:36:13 -0500

    binary search

---
+++ leetcode/Binary_Search/744. Find Smallest Letter Greater Than Target (Easy).java
@@ -0,0 +1,17 @@
+package Binary_Search;
+
+class FindSmallestLetterGreaterThanTarget {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+        int l = 0, h = n - 1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (letters[mid] <= target) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return l < n ? letters[l] : letters[0];
+    }
+}
