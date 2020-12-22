5679170a46bf46f32e42873ba199416e2e46a3fc
code7cs <code7cs@gmail.com>
2020-12-22 17:01:41 -0500

    js

---
+++ leetcode/Others/js-assessment.js
@@ -0,0 +1,74 @@
+// const res = [];
+// function check(par1, ...par2) {
+// 	return res.push(par1, par2);
+// }
+
+// console.log(check('Hello', 'world'));
+
+// let output = [];
+// var component = (...par1) => {
+// 	output = [ ...par1 ];
+// 	return output.filter((a) => a.includes('co'));
+// };
+
+// console.log(component('es6', 'js', 'coffee'));
+
+// let emp = {
+//     empName: {
+//         fn: 'Jogn',
+//         mn: 'Tm',
+//         ln: 'Mark'
+//     }
+
+// }
+// let {fn: mn, ln} = emp.empName;
+// console.log(mn)
+
+// var temp = "8080";
+// if (true) {
+//     const temp = 8080;
+//     console.log(typeof temp);
+// }
+
+// let eid = 'T1105';
+// let emp = {
+//     eid,
+//     getEmpDetails() {
+//         return this.eid;
+//     },
+//     getDetails() {
+//         setInterval(()=>{return this.eid}, 1000);
+//     }
+// }
+// console.log(emp.getEmpDetails())
+// console.log(emp.getDetails())
+
+// let sdata = {
+//     name: "jogn",
+//     country: "India"
+// };
+// var d = "age";
+// sdata[d] = 25;
+// var d = "city";
+// sdata.d = "Pune";
+// console.log(sdata.age);
+// console.log(sdata.city);
+
+// let marr = new Array([1, 2, 3]);
+// marr.delete(1);
+// console.log(marr)
+
+// new Promise(function(resolve, reject) {
+// 	let b;
+// 	setTimeout((com = (a = 5) => resolve(a + b)), 1000);
+// 	b = 25;
+// }).then(function(data) {
+// 	console.log(data);
+// });
+
+// let states = [];
+// states.push(['Rihana', {
+//     eid: 'd',
+// }])
+
+// console.log(...states)
