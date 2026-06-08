a0d698e74cccb49ece83c3e89dcd076e178ae0f7
HF-ALIENWARE\hanfan <code7cs@gmail.com>
2026-06-08 16:48:00 -0400

    Add Tic-Tac-Toe demo

--- angular-demos/src/app/app.routes.ts
+++ angular-demos/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CryptoConverterComponent } from '../crypto-converter/crypto-converter.component';
 import { MemoryGameComponent } from '../memory-game/memory-game.component';
+import { TicTacToeComponent } from '../tic-tac-toe/tic-tac-toe.component';
 import { UndoableCounterComponent } from '../undoable-counter/undoable-counter.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'memory-game', component: MemoryGameComponent },
+  { path: 'tic-tac-toe', component: TicTacToeComponent },
   { path: 'undoable-counter', component: UndoableCounterComponent },
   { path: 'crypto-converter', component: CryptoConverterComponent },
   { path: '**', redirectTo: '' },
