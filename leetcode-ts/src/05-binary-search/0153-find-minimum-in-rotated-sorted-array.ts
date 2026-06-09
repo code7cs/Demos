957219d70c0c0581d80660cbaffaa17b53924cc0
code7cs <code7cs@gmail.com>
2026-06-09 02:30:11 -0400

    Match rotated array minimum solution approach

--- leetcode-ts/src/05-binary-search/0153-find-minimum-in-rotated-sorted-array.ts
+++ leetcode-ts/src/05-binary-search/0153-find-minimum-in-rotated-sorted-array.ts
@@ -9,20 +9,27 @@
 import { runTests } from "../types.js";
 
 function findMin(nums: number[]): number {
+  let res = nums[0];
   let left = 0;
   let right = nums.length - 1;
 
-  while (left < right) {
-    const mid = left + Math.floor((right - left) / 2);
+  while (left <= right) {
+    if (nums[left] < nums[right]) {
+      res = Math.min(res, nums[left]);
+      break;
+    }
 
-    if (nums[mid] > nums[right]) {
+    const mid = left + Math.floor((right - left) / 2);
+    res = Math.min(res, nums[mid]);
+
+    if (nums[mid] >= nums[left]) {
       left = mid + 1;
     } else {
-      right = mid;
+      right = mid - 1;
     }
   }
 
-  return nums[left];
+  return res;
 }
 
 runTests("findMin", findMin as (...args: unknown[]) => unknown, [
